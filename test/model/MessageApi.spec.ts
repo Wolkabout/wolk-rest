ef081693195cd17530de6f75bc3242f089e4738b
Mladen Ružičić <ruzicic@gmail.com>
2018-08-08 17:19:59 +0200

    :sparkles: :white_check_mark: Add Message API w/ tests

--- test/model/MessageApi.spec.ts
+++ test/model/MessageApi.spec.ts
@@ -1,28 +1,66 @@
 import { expect } from 'chai';
 import WolkREST from '../../src';
-import environment from '../resources/environment';
-import user from '../authentication/resources/user';
+import { getAuthenticatedWolkRestInstance } from '../utils';
 
 describe('Message API', () => {
   let wolkRest: WolkREST;
 
   before(async () => {
-    wolkRest = new WolkREST(environment.baseURL);
-    await wolkRest.auth().emailSignIn({
-      username: user.valid.email,
-      password: user.valid.password
-    });
+    wolkRest = await getAuthenticatedWolkRestInstance();
   });
 
   context('[GET] /api/messages', async () => {
-    // before(async () => {
-    // });
-
-    it('Should get paged messages', async () => {
-      const { data: brandDetails, status } = await wolkRest.message().pageMessages();
+    it('Should get paged messages, without any params', async () => {
+      const { data: pageMessages, status } = await wolkRest.message().pageMessages();
 
       expect(status).to.equal(200);
-      // expect(brandDetails).to.deep.include(brandMock.BRANDING_1);
+      expect(pageMessages.content).to.be.an.instanceof(Array);
+    });
+
+    it('Should get paged messages with params', async () => {
+      const params = {
+        since: 0,
+        to: 0,
+        type: 'ALARM',
+        query: '',
+        read: false
+      };
+      const { data: pageMessages, status } = await wolkRest.message().pageMessages(params);
+
+      expect(status).to.equal(200);
+      expect(pageMessages.content).to.be.an.instanceof(Array);
+    });
+
+    it('Should get unread alarms', async () => {
+      const { data: unreadAlarms, status } = await wolkRest.message().unreadAlarms();
+
+      expect(status).to.equal(200);
+      expect(unreadAlarms).to.be.an.instanceof(Array);
+    });
+  });
+
+  context('[PUT] /api/messages/markAsRead', async () => {
+    it('Should mark zero messages as read', async () => {
+      const { status } = await wolkRest.message().markAsRead([]);
+
+      expect(status).to.equal(200);
+    });
+  });
+
+  context('[GET] /api/messages/unreadCount?type=ALARM', async () => {
+    it('Should get number of unread ALARM messages', async () => {
+      const { data: unreadAlarmsCount, status } = await wolkRest.message().countUnreadAlarms();
+
+      expect(status).to.equal(200);
+      expect(unreadAlarmsCount).to.be.a('number');
+    });
+  });
+
+  context('[PUT] /api/messages/markAsReadAll', async () => {
+    it('Should mark all messages as read', async () => {
+      const { status } = await wolkRest.message().markAsReadAll();
+
+      expect(status).to.equal(200);
     });
   });
 
