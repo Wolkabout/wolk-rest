44d91b5d5427afa0b60b12566d68583cc6481486
Mladen Ružičić <ruzicic@gmail.com>
2018-08-09 08:23:00 +0200

    Use getAuthenticatedWolkRestInstance (#13)
    * :wrench: Use getAuthenticatedWolkRestInstance

--- test/device/template/DeviceManifestApi.spec.ts
+++ test/device/template/DeviceManifestApi.spec.ts
@@ -1,24 +1,18 @@
 import { expect } from 'chai';
 import WolkREST from '../../../src';
-import environment from '../../resources/environment';
 import DeviceManifest from '../../../src/device/template/model/DeviceManifest';
 import fromResource from './resources/deviceManifest';
-import fromAuthUser from '../../authentication/resources/user';
+import { getAuthenticatedWolkRestInstance } from '../../utils';
 
 describe('DeviceTemplate/Manifest API', () => {
   let wolkRest: WolkREST;
 
   before(async () => {
-    wolkRest = new WolkREST(environment.baseURL);
-    await wolkRest.auth().emailSignIn({
-      username: fromAuthUser.valid.email,
-      password: fromAuthUser.valid.password
-    });
+    wolkRest = await getAuthenticatedWolkRestInstance();
   });
 
   it('[GET] /api/deviceManifests', async () => {
-    const deviceManifest: DeviceManifest = await wolkRest.deviceManifest()
-    .getPublicDeviceManifest(fromResource.name);
+    const deviceManifest: DeviceManifest = await wolkRest.deviceManifest().getPublicDeviceManifest(fromResource.name);
 
     expect(deviceManifest).to.deep.include('Pyhton');
   });
