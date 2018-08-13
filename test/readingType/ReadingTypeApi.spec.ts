f5d274b00bf36edd9fa7a6e0d6df264466c34feb
Radovan Skendzic <rskendzic@execom.eu>
2018-08-13 14:53:41 +0200

    :fire: :hammer: Reading type API

--- test/readingType/ReadingTypeApi.spec.ts
+++ test/readingType/ReadingTypeApi.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import WolkREST from '../../src';
 import { getAuthenticatedWolkRestInstance } from '../utils';
+import * as fromResources from './resources/index';
 
 describe('ReadingType API', () => {
   let wolkRest: WolkREST;
@@ -11,10 +12,22 @@ describe('ReadingType API', () => {
 
   context('[GET] /api/readingTypes', async () => {
     it('Should get system reading types', async () => {
-      const { data: readingTypes, status } = await wolkRest.readingType().getList();
+      const { status } = await wolkRest.readingType().getList();
 
       expect(status).to.equal(200);
-      expect(readingTypes).to.be.an.instanceof('array');
+    });
+  });
+
+  context('[GET] /api/readingTypes', async () => {
+    let customUnitId: number;
+    it('Should create custom unit', async () => {
+      const { status, data: unitId } = await wolkRest.readingType().createUnit(fromResources.unitDto);
+      customUnitId = unitId;
+      expect(status).to.equal(201);
+    });
+
+    after(async () => {
+      await wolkRest.unit().deleteUnit(customUnitId);
     });
   });
 
---
+++ test/readingType/resources/index.ts
@@ -0,0 +1,9 @@
+import { Unit } from '../../../src/unit/model';
+
+export const unitDto: Unit =  {
+  symbol: 'WRT',
+  readingTypeId: 54,
+  inUse: false,
+  system: 'CUSTOM',
+  name: 'WRT - CUSTOM UNIT'
+};
