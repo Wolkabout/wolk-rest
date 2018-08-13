093cd4c39884be0a264ba9e019f745b0178e5f95
Mladen Ružičić <ruzicic@gmail.com>
2018-08-13 09:47:52 +0200

    :pencil2: Comments cleanup

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,11 +3,10 @@ import { config } from 'dotenv';
 const configLoaded = config();
 
 if (configLoaded.error) {
-  // console.log('jbga', configLoaded.error);
+  // tslint:disable-next-line: no-console
+  console.error('Failed to load config.', configLoaded.error);
   throw configLoaded.error;
 }
-
-// console.log('Config loded:', configLoaded);
 
 export const WA_TEST_BASEURL = String(process.env.WA_TEST_BASEURL);
 export const WA_TEST_USER = String(process.env.WA_TEST_USER);
