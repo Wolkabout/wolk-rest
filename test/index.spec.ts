a0aa5f56e4cb284369859e71a0b6a43bac149d21
Mladen Ružičić <ruzicic@gmail.com>
2018-08-16 11:42:50 +0200

    :white_check_mark: Add session storage tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,52 @@
 import { expect } from 'chai';
-import WolkREST from '../src';
+import WolkREST, { BrowserLocalStorage, InMemoryStorage } from '../src';
+import { SessionStorage } from '../src/model/SessionStorage';
 
 describe('Index', () => {
   it('Should export WolkREST Client', () => {
     expect(WolkREST).to.equal(WolkREST);
+  });
+
+  it('Should export InMemoryStorage', () => {
+    expect(InMemoryStorage).to.be.a('function');
+  });
+
+  it('Should export BrowserLocalStorage', () => {
+    expect(BrowserLocalStorage).to.be.a('function');
+  });
+
+  it('Should initialize WolkREST Client with default storage', () => {
+    const instance = new WolkREST('https://example.com');
+
+    expect(instance).to.be.an('object');
+    expect(instance.auth()).to.not.throw;
+  });
+
+  it('Should initialize WolkREST Client with custom storage', () => {
+    const customStorage: SessionStorage = {
+      getToken(): string {
+        return '';
+      },
+      setToken(token: string): void {
+      },
+      clearToken(): void {
+      }
+    };
+    const instance = new WolkREST('https://example.com', customStorage);
+
+    expect(instance).to.be.an('object');
+    expect(instance.auth()).to.not.throw;
+  });
+
+  it('Should fail to initialize WolkREST Client with BrowserLocalStorage storage', () => {
+    try {
+      const localStorage = new BrowserLocalStorage();
+      new WolkREST('https://example.com', localStorage);
+    } catch (error) {
+      expect(error.message).to.be.equal(
+        'BrowserLocalStorage can only be initialized in browser with Local Storage support.'
+      );
+
+    }
   });
 });
